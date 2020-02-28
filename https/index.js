9e6d230ae1fc8017bfd434f38bf976784cdb1af1
Shubham Srivastava <shubhams@geekyants.com>
2020-02-28 15:19:09 +0530

    moved express app in https

---
+++ https/index.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', usersRouter);
+
+app.use(async (req, res)=>{
+    res.status(404);
+    res.send({
+        hasError: true,
+        message: 'not found'
+    });
+});
+
+app.use(async (error, req, res, next) =>{
+    res.send({
+        hasError : true,
+        message: error.message,
+    });
+});
+
+module.exports = app;
+
+
